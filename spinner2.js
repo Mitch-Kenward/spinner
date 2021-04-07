3d727063b94d033999be6a9be62559aa1475c867
Mitch-Kenward <mkenward29@gmail.com>
2021-04-07 21:34:34 +0000

    too much fun

---
+++ spinner2.js
@@ -0,0 +1,20 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setInterval(() => {
+setTimeout(() => {
+  process.stdout.write('\r|  ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/  ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-  ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\  ');
+}, 700);
+}, 900);
+
